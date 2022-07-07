4e7ae1c2494ff17c7be5c8783ab9d56b11b8f46b
ViktorSivashev <viktor.sivashev@mail.ru>
2022-07-07 16:36:07 +0300

    add Example 006 Giri

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 39;
+int b = 15;
+int c = 20;
+int d = 78;
+int e = 41;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max =");
+Console.WriteLine(max);
