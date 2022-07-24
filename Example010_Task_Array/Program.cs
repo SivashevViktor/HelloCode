1674bc8fd8e81a51dce730d11da8ed9d0c4a1a56
ViktorSivashev <viktor.sivashev@mail.ru>
2022-07-24 17:27:33 +0300

    example010

---
+++ Example010_Task_Array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 19, 22, 38, 88, 54, 56, 61, 47, 88 };
+
+int n = array.Length;
+int find = 88;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
